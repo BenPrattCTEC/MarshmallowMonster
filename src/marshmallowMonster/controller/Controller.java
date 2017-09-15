f72da77326449483d33c064a1d7f727857f2be44
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-15 11:00:00 -0700

    added Monster class and implimented it in start()

--- src/marshmallowMonster/controller/Controller.java
+++ src/marshmallowMonster/controller/Controller.java
@@ -1,14 +1,14 @@
 package marshmallowMonster.controller;
 
+import marshmallowMonster.model.Monster;
+
 public class Controller {
 	
-	public Controller() {
-		
-	}
-	
 	public void start() {
+	
+		Monster monsterPhilip = new Monster("Philip", 2, 2, 4, true);
 		
-		
+		System.out.println(monsterPhilip.toString());
 		
 	}
 	
