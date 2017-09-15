0397a613c9b712960bbec2cb9e29fbc4c61638b6
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-15 12:05:55 -0700

    added setters for Monster() and fixed minor bugs

--- src/marshmallowMonster/controller/Controller.java
+++ src/marshmallowMonster/controller/Controller.java
@@ -8,13 +8,16 @@ public class Controller {
 		
 		//Monster(name, eyeCount, armCount, tenticleAmount, hasBloop)
 		Monster[] monsters = {
-			new Monster("Philip", 2, 2, 4, true),
-			new Monster("Bob", 1, 3, 0, true)
+			new Monster("Philip", 2, 2, 2, true),
+			new Monster("Bob", 1, 3, 0, true),
 		};
+		
+		monsters[0].eat();
 		
 		for (int i = 0; i < monsters.length; i++) {
 			System.out.println(monsters[i].toString());
 		}
-		System.out.println("Number of Monsters: " + Monster.getNumberOfMonsters());
+		System.out.println();
+		System.out.println("Number of  Living Monsters: " + Monster.getNumberOfMonsters());
 	}
 }
--- src/marshmallowMonster/model/Monster.java
+++ src/marshmallowMonster/model/Monster.java
@@ -7,7 +7,20 @@ public class Monster {
 	private String name;
 	private boolean hasBloop;
 	
-	private static int numberOfMonsters=0;
+	private boolean isInitialized;
+	private boolean isEaten = false;
+	
+	private int monsterID;
+	
+	private static int numberOfMonsters = 0;
+	private static int IDAccumulator = 0;
+	
+	public Monster() {
+		isInitialized = false;
+		monsterID = numberOfMonsters;
+		numberOfMonsters++;
+		IDAccumulator++;
+	}
 	
 	public Monster(String name, int eyeCount, int armCount, int tenticleAmount, boolean hasBloop) {
 		
@@ -16,12 +29,42 @@ public class Monster {
 		this.tenticleAmount = tenticleAmount;
 		this.name = name;
 		this.hasBloop = hasBloop;
+		monsterID = IDAccumulator;
 		numberOfMonsters++;
+		IDAccumulator++;
+		isInitialized = true;
 		
 	}
 	
-	static public int getNumberOfMonsters() {
-		return numberOfMonsters;
+	public void Initialize(String name, int eyeCount, int armCount, int tenticleAmount, boolean hasBloop) {
+		
+		this.armCount = armCount;
+		this.eyeCount = eyeCount;
+		this.tenticleAmount = tenticleAmount;
+		this.name = name;
+		this.hasBloop = hasBloop;
+		isInitialized = true;
+		
+	}
+	
+	public void setArmCount(int count) {
+		this.armCount = count;
+	}
+	
+	public void setEyeCount(int count) {
+		this.eyeCount = count;
+	}
+	
+	public void setTenticleAmount(int amount) {
+		this.tenticleAmount = amount;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void sethasBloop(boolean hasBloop) {
+		this.hasBloop = hasBloop;
 	}
 	
 	public int getArmCount() {
@@ -44,9 +87,32 @@ public class Monster {
 		return hasBloop;
 	}
 	
+	public int getID() {
+		return monsterID;
+	}
+	
+	static public int getNumberOfMonsters() {
+		return numberOfMonsters;
+	}
+	
+	public void eat() {
+		isEaten = true;
+		numberOfMonsters--;
+	}
+	
 	public String toString() {
-		return "Name: " + name + "\n" + "Number of eyes: " + eyeCount + "\n" + "Number of tenticles: " + tenticleAmount
-				+ "\n" + "Number of Arms: " + armCount + "\n" + "Bloop state: " + hasBloop + "\n";
+		if (isInitialized) {
+			if (!isEaten)
+				return "ID: " + monsterID + "\n" + "Name: " + name + "\n" + "Number of eyes: " + eyeCount + "\n"
+						+ "Number of tenticles: " + tenticleAmount + "\n" + "Number of Arms: " + armCount + "\n"
+						+ "Bloop state: " + hasBloop + "\n";
+			else {
+				return "ID: " + monsterID + "\n" + name + " Has been Eaten" + "\n";
+			}
+			
+		}
+		return "ID: " + monsterID + "\n" + "Monster not properly initialized" + "\n";
+		
 	}
 	
 }
