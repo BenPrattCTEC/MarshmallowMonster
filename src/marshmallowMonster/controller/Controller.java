90d303b3cd62d0c706245a5f8ae0fe5cba2a1f97
Bcpratt2000 <bcpratt2000@gmail.com>
2017-10-16 12:12:46 -0600

    added a menu system

--- src/marshmallowMonster/controller/Controller.java
+++ src/marshmallowMonster/controller/Controller.java
@@ -7,201 +7,233 @@ import java.lang.NumberFormatException;
 import marshmallowMonster.view.GuiController;
 import marshmallowMonster.model.Monster;
 
-public class Controller {
-	
+public class Controller
+{
+
 	GuiController gui = new GuiController();
-	
-	public void start() {
-		
+
+	public void start()
+	{
+
+		String awnser = "";
+
 		ArrayList<Monster> monsters = new ArrayList<Monster>();
-		
+
 		monsters.add(new Monster("Philip", 2, 2, 2, true));
 		monsters.add(new Monster("Bob", 1, 3, 0, true));
 		monsters.add(new Monster());
-		
-		// eats the first monster
-		monsters.get(0).eat();
-		
-		// create monster #3 from blank declared earlier
-//		interactiveMonsterCreation(monsters.get(2));
-		
-		
-		// eats eyes of monster #2
-//		boolean correctInput = false;
-//		String tempString;
-//		while (!correctInput) {
-//			tempString = gui.displayQuestion(monsters.get(1).toString() + "\nHow many eyes would you like to eat?");
-//			if (isValidInt(tempString)) {
-//				eatPartNoFeedback(monsters.get(1), "eye", Math.abs(Integer.parseInt(tempString)));
-//				correctInput = true;
-//			}
-//			else if (tempString == null) {
-//				correctInput = true;
-//			}
-//			else {
-//				gui.displayText("Entered value was not an positive integer, try again");
-//			}
-//		}
-		
-		// prints all monsters
-		printMonsterArrayListToPopup(monsters);
-	}
-	
-	private void printMonsterArrayListToPopup(List<Monster> monsters) {
-		String tempString = "";
-//		for (int i = 0; i < monsters.size(); i++) {
-//			tempString += monsters.get(i).toString() + "\n";
-//		}
-		for(Monster current : monsters){
-			tempString+=current.toString() + "\n";
-		}
-		gui.displayText(tempString);
-	}
-	
-	private void eatPart(Monster monster, String part, double amount) {
-		
-		// valid parts: arm, eye, tentacle, bloop
-		
-		if (amount < 0) {
-			System.out.println("Error: amount is less than 0");
-		}
-		else if (amount == 0) {
-			System.out.println("Error: amount is 0");
-		}
-		else {
-			switch (part.toLowerCase()) {
-				case "arm":
-					monster.setArmCount(monster.getArmCount() - (int) amount);
-					System.out.println("you ate " + amount + " arm(s)");
-					if (monster.getArmCount() < 0) {
-						monster.setArmCount(0);
-					}
-					System.out.println("the monster now has " + monster.getArmCount());
-					break;
-				case "eye":
-					monster.setEyeCount(monster.getEyeCount() - (int) amount);
-					System.out.println("you ate " + amount + " eye(s)");
-					if (monster.getEyeCount() < 0) {
-						monster.setEyeCount(0);
-					}
-					System.out.println("the monster now has " + monster.getEyeCount());
-					break;
-				case "tentacle":
-					monster.setTentacleAmount(monster.getTentacleAmount() - amount);
-					System.out.println("you ate " + amount + " tentacle(s)");
-					if (monster.getTentacleAmount() < 0) {
-						monster.setTentacleAmount(0);
-					}
-					System.out.println("the monster now has " + monster.getTentacleAmount());
-					break;
-				case "bloop":
-					if (monster.gethasBloop()) {
-						monster.setHasBloop(false);
-						System.out.println("The monster now no longer has a bloop");
-					}
-					else {
-						System.out.println("The monster Doesn't have a bloop to eat");
-					}
-					break;
+
+		while (true)
+		{
+			awnser = gui.displayQuestion("What would you like to do?\n1) Make Monster\n2) Remove Monster\n3) Print Monsters");
+			if (awnser.equals("1"))
+			{
+				monsters.add(new Monster());
+				interactiveMonsterCreation(monsters.get(monsters.size() - 1));
+			} else if (awnser.equals("2"))
+			{
+				awnser = gui.displayQuestion(printMonsterArrayListToString(monsters));
+				monsters.remove(Integer.parseInt(awnser));
+			} else
+			{
+				gui.displayText(printMonsterArrayListToString(monsters));
 			}
 		}
+		// boolean correctInput = false;
+		// String tempString;
+		// while (!correctInput) {
+		// tempString = gui.displayQuestion(monsters.get(1).toString() + "\nHow many eyes would you like to
+		// eat?");
+		// if (isValidInt(tempString)) {
+		// eatPartNoFeedback(monsters.get(1), "eye", Math.abs(Integer.parseInt(tempString)));
+		// correctInput = true;
+		// }
+		// else if (tempString == null) {
+		// correctInput = true;
+		// }
+		// else {
+		// gui.displayText("Entered value was not an positive integer, try again");
+		// }
+		// }
+
+		// prints all monsters
 	}
-	
-	private void eatPartNoFeedback(Monster monster, String part, double amount) {
+
+	private String printMonsterArrayListToString(List<Monster> monsters)
+	{
+		String tempString = "";
+		// for (int i = 0; i < monsters.size(); i++) {
+		// tempString += monsters.get(i).toString() + "\n";
+		// }
+		for (Monster current : monsters)
+		{
+			tempString += current.toString() + "\n";
+		}
+		return tempString;
+	}
+
+	private void eatPart(Monster monster, String part, double amount)
+	{
+
 		// valid parts: arm, eye, tentacle, bloop
-		
-		switch (part.toLowerCase()) {
+
+		if (amount < 0)
+		{
+			System.out.println("Error: amount is less than 0");
+		} else if (amount == 0)
+		{
+			System.out.println("Error: amount is 0");
+		} else
+		{
+			switch (part.toLowerCase())
+			{
 			case "arm":
 				monster.setArmCount(monster.getArmCount() - (int) amount);
-				if (monster.getArmCount() < 0) {
+				System.out.println("you ate " + amount + " arm(s)");
+				if (monster.getArmCount() < 0)
+				{
 					monster.setArmCount(0);
 				}
+				System.out.println("the monster now has " + monster.getArmCount());
 				break;
 			case "eye":
 				monster.setEyeCount(monster.getEyeCount() - (int) amount);
-				if (monster.getEyeCount() < 0) {
+				System.out.println("you ate " + amount + " eye(s)");
+				if (monster.getEyeCount() < 0)
+				{
 					monster.setEyeCount(0);
 				}
+				System.out.println("the monster now has " + monster.getEyeCount());
 				break;
 			case "tentacle":
 				monster.setTentacleAmount(monster.getTentacleAmount() - amount);
-				if (monster.getTentacleAmount() < 0) {
+				System.out.println("you ate " + amount + " tentacle(s)");
+				if (monster.getTentacleAmount() < 0)
+				{
 					monster.setTentacleAmount(0);
 				}
+				System.out.println("the monster now has " + monster.getTentacleAmount());
 				break;
 			case "bloop":
-				if (monster.gethasBloop()) {
+				if (monster.gethasBloop())
+				{
 					monster.setHasBloop(false);
+					System.out.println("The monster now no longer has a bloop");
+				} else
+				{
+					System.out.println("The monster Doesn't have a bloop to eat");
 				}
 				break;
+			}
 		}
 	}
-	
-	private void interactiveMonsterCreation(Monster monster) {
-		
+
+	private void eatPartNoFeedback(Monster monster, String part, double amount)
+	{
+		// valid parts: arm, eye, tentacle, bloop
+
+		switch (part.toLowerCase())
+		{
+		case "arm":
+			monster.setArmCount(monster.getArmCount() - (int) amount);
+			if (monster.getArmCount() < 0)
+			{
+				monster.setArmCount(0);
+			}
+			break;
+		case "eye":
+			monster.setEyeCount(monster.getEyeCount() - (int) amount);
+			if (monster.getEyeCount() < 0)
+			{
+				monster.setEyeCount(0);
+			}
+			break;
+		case "tentacle":
+			monster.setTentacleAmount(monster.getTentacleAmount() - amount);
+			if (monster.getTentacleAmount() < 0)
+			{
+				monster.setTentacleAmount(0);
+			}
+			break;
+		case "bloop":
+			if (monster.gethasBloop())
+			{
+				monster.setHasBloop(false);
+			}
+			break;
+		}
+	}
+
+	private void interactiveMonsterCreation(Monster monster)
+	{
+
 		int eyeCount = 0, armCount = 0;
 		double tenticleAmount = 0;
 		String name = "";
 		boolean hasBloop = false;
-		
+
 		boolean isComplete = false;
-		
-		while (!isComplete) {
-			
-			try {
+
+		while (!isComplete)
+		{
+
+			try
+			{
 				name = gui.displayQuestion("ID: " + monster.getID() + "\nEnter Name");
-				eyeCount = Integer
-						.parseInt(gui.displayQuestion("ID: " + monster.getID() + "\n(Integer) Enter number of eyes"));
-				tenticleAmount = Double.parseDouble(
-						gui.displayQuestion("ID: " + monster.getID() + "\n(Double) Enter number of Tenticles"));
-				armCount = Integer
-						.parseInt(gui.displayQuestion("ID: " + monster.getID() + "\n(Integer) Enter number of Arms"));
-				hasBloop = Boolean.parseBoolean(
-						gui.displayQuestion("ID: " + monster.getID() + "\n(Boolean) Does it hava a Bloop?"));
-				
+				eyeCount = Integer.parseInt(gui.displayQuestion("ID: " + monster.getID() + "\n(Integer) Enter number of eyes"));
+				tenticleAmount = Double.parseDouble(gui.displayQuestion("ID: " + monster.getID() + "\n(Double) Enter number of Tenticles"));
+				armCount = Integer.parseInt(gui.displayQuestion("ID: " + monster.getID() + "\n(Integer) Enter number of Arms"));
+				hasBloop = Boolean.parseBoolean(gui.displayQuestion("ID: " + monster.getID() + "\n(Boolean) Does it hava a Bloop?"));
+
 				isComplete = true;
-			}
-			catch (Exception e) {
+			} catch (Exception e)
+			{
 				gui.displayText("\nField not entered correctly, Make sure it was the correct data type\n");
 			}
-			
+
 		}
-		
+
 		monster.initialize(name, eyeCount, armCount, tenticleAmount, hasBloop);
 	}
-	
+
 	// helpers-----------------------------------------------------------------------------------------------
-	
-	private boolean isValidInt(String sample) {
-		try {
+
+	private boolean isValidInt(String sample)
+	{
+		try
+		{
 			Integer.parseInt(sample);
-		}
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e)
+		{
 			return false;
 		}
-		
+
 		return true;
 	}
-	
-	private boolean isValidDouble(String sample) {
-		try {
+
+	private boolean isValidDouble(String sample)
+	{
+		try
+		{
 			Double.parseDouble(sample);
-		}
-		catch (NumberFormatException e) {
+		} catch (NumberFormatException e)
+		{
 			return false;
 		}
-		
+
 		return true;
 	}
-	
-	private boolean isValidBoolean(String sample) {
-		switch (sample.toLowerCase()) {
-			case "true":
-				return true;
-			case "false":
-				return true;
+
+	private boolean isValidBoolean(String sample)
+	{
+		switch (sample.toLowerCase())
+		{
+		case "true":
+			return true;
+		case "false":
+			return true;
 		}
 		return false;
 	}
-	
+
 }
